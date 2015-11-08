721e9920c9f8fc20905f7c20913c77f325ba63fd
kartikeya kirar <kirar.kartikeya1@gmail.com>
2015-11-09 01:37:14 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,11 +1,17 @@
-consumption <- read.csv("./household_power_consumption.txt",sep=";",na.strings='?',header=TRUE,colClasses = c("character",rep("factor",1),rep("numeric",7)))
-selection<-consumption[consumption$Date %in% c("1/2/2007","2/2/2007"),]
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-plot(strptime(paste(selection$Date,selection$Time),"%d/%m/%Y %H:%M:%S"),
-     selection$Sub_metering_1,ylab="Energy sub metering",xlab="",type="l")
-lines(strptime(paste(selection$Date,selection$Time),"%d/%m/%Y %H:%M:%S"),selection$Sub_metering_2,col="red")
-lines(strptime(paste(selection$Date,selection$Time),"%d/%m/%Y %H:%M:%S"),selection$Sub_metering_3,col="blue")
-legend(cex=0.7,"topright", lty=c(rep(1,3)), col = c("black","red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
-dev.copy(png, file = "plot3.png")  ## copy the plot to a PNG file
-dev.off()  ## close PNG device
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
